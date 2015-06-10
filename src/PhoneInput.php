413c4aac1cd8e6c144a9f112df9e92ebf8dee7a5
Borales <bordun.alexandr@gmail.com>
2015-06-10 19:40:31 +0200

    Widget and Asset Bundle

---
+++ src/PhoneInput.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace borales\extensions\phoneInput;
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\widgets\InputWidget;
+
+/**
+ * Widget of the phone input
+ * @package borales\extensions\phoneInput
+ */
+class PhoneInput extends InputWidget
+{
+    /** @var string HTML tag type of the widget input ("tel" by default) */
+    public $htmlTagType = 'tel';
+    /** @var array Default widget options of the HTML tag */
+    public $defaultOptions = ['autocomplete' => "off"];
+    /** @var array Options of the JS-widget */
+    public $jsOptions = [];
+
+    public function init()
+    {
+        parent::init();
+        PhoneInputAsset::register($this->view);
+        $id = ArrayHelper::getValue($this->options, 'id');
+        $jsOptions = $this->jsOptions ? Json::encode($this->jsOptions) : "";
+        $this->view->registerJs("$('#$id').intlTelInput($jsOptions);");
+    }
+
+    /**
+     * @return string
+     */
+    public function run()
+    {
+        $options = ArrayHelper::merge($this->defaultOptions, $this->options);
+        if ($this->hasModel()) {
+            return Html::activeInput($this->htmlTagType, $this->model, $this->attribute, $options);
+        }
+        return Html::input($this->htmlTagType, $this->name, $this->value, $options);
+    }
+}
---
+++ src/PhoneInputAsset.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace borales\extensions\phoneInput;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset Bundle of the phone input widget. Registers required CSS and JS files.
+ * @package borales\extensions\phoneInput
+ */
+class PhoneInputAsset extends AssetBundle
+{
+    /** @var string */
+    public $sourcePath = '@bower/intl-tel-input';
+    /** @var array */
+    public $css = ['build/css/intlTelInput.css'];
+    /** @var array */
+    public $js = [
+        'lib/libphonenumber/build/utils.js',
+        'build/js/intlTelInput.min.js',
+    ];
+    /** @var array */
+    public $depends = ['yii\web\JqueryAsset'];
+}
