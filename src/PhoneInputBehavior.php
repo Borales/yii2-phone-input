12c2273c9edb0cbd6a97eafcb8d8fa64ddc6482b
Borales <bordun.alexandr@gmail.com>
2015-06-10 19:44:47 +0200

    Behavior

---
+++ src/PhoneInputBehavior.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace borales\extensions\phoneInput;
+
+use libphonenumber\NumberParseException;
+use libphonenumber\PhoneNumberFormat;
+use libphonenumber\PhoneNumberUtil;
+use yii\base\Event;
+use yii\behaviors\AttributeBehavior;
+use yii\db\BaseActiveRecord;
+
+/**
+ * Behavior of the phone input widget. Auto-formats the phone value for the JS-widget.
+ * @package borales\extensions\phoneInput
+ */
+class PhoneInputBehavior extends AttributeBehavior
+{
+    /** @var string */
+    public $phoneAttribute = 'phone';
+
+    public function init()
+    {
+        parent::init();
+        if (empty($this->attributes)) {
+            $this->attributes = [
+                BaseActiveRecord::EVENT_BEFORE_INSERT => $this->phoneAttribute,
+                BaseActiveRecord::EVENT_BEFORE_UPDATE => $this->phoneAttribute,
+                BaseActiveRecord::EVENT_AFTER_FIND => $this->phoneAttribute,
+            ];
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function events()
+    {
+        $events = parent::events();
+        $events[BaseActiveRecord::EVENT_AFTER_FIND] = 'formatAttributes';
+        return $events;
+    }
+
+    /**
+     * Evaluates the attribute value and assigns it to the current attributes.
+     * @param Event $event
+     */
+    public function evaluateAttributes($event)
+    {
+        if (!empty($this->attributes[$event->name])) {
+            $attributes = (array)$this->attributes[$event->name];
+            foreach ($attributes as $attribute) {
+                if (is_string($attribute) && $this->owner->$attribute) {
+                    try {
+                        $phoneValue = $this->getPhoneUtil()->parse($this->owner->$attribute, null);
+                        $this->owner->$attribute = $this->getPhoneUtil()->format($phoneValue, PhoneNumberFormat::E164);
+                    } catch (NumberParseException $e) {
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @param $event
+     */
+    public function formatAttributes($event)
+    {
+        if (!empty($this->attributes[$event->name])) {
+            $attributes = (array)$this->attributes[$event->name];
+            foreach ($attributes as $attribute) {
+                if (is_string($attribute) && $this->owner->$attribute) {
+                    try {
+                        $phoneValue = $this->getPhoneUtil()->parse($this->owner->$attribute, null);
+                        $this->owner->$attribute = $this->getPhoneUtil()->format($phoneValue,
+                            PhoneNumberFormat::INTERNATIONAL);
+                    } catch (NumberParseException $e) {
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @return PhoneNumberUtil
+     */
+    protected function getPhoneUtil()
+    {
+        return PhoneNumberUtil::getInstance();
+    }
+}
